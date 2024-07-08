a832b16030db1e9bf04e3c6a32b7da2020ecad43
Amankhan044 <amankhan06a@gmail.com>
2024-07-08 12:57:41 +0500

    screens added

--- lib/models/main_view_provider.dart
+++ lib/models/main_view_provider.dart
@@ -1,11 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:multi_vendor_app_provider/views/buyers/nav_screen/account_view.dart';
+import 'package:multi_vendor_app_provider/views/buyers/nav_screen/cart_view.dart';
+import 'package:multi_vendor_app_provider/views/buyers/nav_screen/category_view.dart';
+import 'package:multi_vendor_app_provider/views/buyers/nav_screen/home_view.dart';
+import 'package:multi_vendor_app_provider/views/buyers/nav_screen/search_view.dart';
+import 'package:multi_vendor_app_provider/views/buyers/nav_screen/store_view.dart';
 
 class MainViewProvider extends ChangeNotifier{
 
 int _pageIndex= 0;
 int get pageIndex=>_pageIndex;
 
-void SetPageValue(int value){
+List<Widget> _listPages=[HomeView(),CategoryView(),StoreView(),CartView(),SearchView(),AccountView()];
+List<Widget> get listPages=>_listPages;
+
+void setPageValue(int value){
   _pageIndex=value;
   notifyListeners();
 }
--- lib/views/buyers/main_view.dart
+++ lib/views/buyers/main_view.dart
@@ -9,48 +9,51 @@ class MainView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      bottomNavigationBar: Consumer<MainViewProvider>(
-        builder: (context, value, child) {
-          return BottomNavigationBar(
-              currentIndex: value.pageIndex,
-              onTap: (v) {
-                value.SetPageValue(v);
-              },
-              unselectedItemColor: Colors.black,
-              selectedItemColor: Colors.yellow.shade900,
-              items: [
-                BottomNavigationBarItem(
-                    icon: Icon(CupertinoIcons.home), label: 'HOME'),
-                BottomNavigationBarItem(
-                  icon: SvgPicture.asset(
-                    'assets/icons/explore.svg',
-                    width: 20,
-                  ),
-                  label: 'CATEGORIES',
-                ),
-                BottomNavigationBarItem(
-                  icon: SvgPicture.asset(
-                    'assets/icons/shop.svg',
-                    width: 20,
-                  ),
-                  label: 'STORE',
-                ),
-                BottomNavigationBarItem(
-                  icon: SvgPicture.asset('assets/icons/cart.svg'),
-                  label: 'CART',
-                ),
-                BottomNavigationBarItem(
-                  icon: SvgPicture.asset('assets/icons/search.svg'),
-                  label: 'SEARCH',
-                ),
-                BottomNavigationBarItem(
-                  icon: SvgPicture.asset('assets/icons/account.svg'),
-                  label: 'ACCOUNT',
-                ),
-              ]);
-        },
-      ),
-    );
+    return Consumer<MainViewProvider>(builder: (context, value, child) {
+      return Scaffold(
+        bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          currentIndex: value.pageIndex,
+          onTap: (v) {
+            value.setPageValue(v);
+          },
+          unselectedItemColor: Colors.black,
+          selectedItemColor: Colors.yellow.shade900,
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(CupertinoIcons.home),
+              label: 'HOME',
+            ),
+            BottomNavigationBarItem(
+              icon: SvgPicture.asset(
+                'assets/icons/explore.svg',
+                width: 20,
+              ),
+              label: 'CATEGORIES',
+            ),
+            BottomNavigationBarItem(
+              icon: SvgPicture.asset(
+                'assets/icons/shop.svg',
+                width: 20,
+              ),
+              label: 'STORE',
+            ),
+            BottomNavigationBarItem(
+              icon: SvgPicture.asset('assets/icons/cart.svg'),
+              label: 'CART',
+            ),
+            BottomNavigationBarItem(
+              icon: SvgPicture.asset('assets/icons/search.svg'),
+              label: 'SEARCH',
+            ),
+            BottomNavigationBarItem(
+              icon: SvgPicture.asset('assets/icons/account.svg'),
+              label: 'ACCOUNT',
+            ),
+          ],
+        ),
+        body: value.listPages[value.pageIndex],
+      );
+    });
   }
 }
