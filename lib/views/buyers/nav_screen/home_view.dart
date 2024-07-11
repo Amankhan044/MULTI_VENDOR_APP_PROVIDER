098432a2ff9b8912c0f72584a60f882a812bb627
Amankhan044 <amankhan06a@gmail.com>
2024-07-11 11:34:13 +0500

    integrated firebase

--- lib/views/buyers/nav_screen/home_view.dart
+++ lib/views/buyers/nav_screen/home_view.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:multi_vendor_app_provider/views/buyers/nav_screen/widgets/banner_widget.dart';
+import 'package:multi_vendor_app_provider/views/buyers/nav_screen/widgets/search_input_widget.dart';
+import 'package:multi_vendor_app_provider/views/buyers/nav_screen/widgets/welcome_text.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Home Screen"),
-    );
+    return Column(children: [
+      WelcomeText(),
+      SizedBox(
+        height: 14,
+      ),
+      SearchInputWidget(),
+      BannerWidget()
+    ]);
   }
 }
---
+++ lib/views/buyers/nav_screen/widgets/banner_widget.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class BannerWidget extends StatelessWidget {
+  const BannerWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Container(
+        height: 140,
+        width: double.infinity,
+        decoration: BoxDecoration(
+            color: Colors.yellow.shade900,
+            borderRadius: BorderRadius.circular(10)),
+        child: PageView(
+          children: [
+            Center(
+              child: Text('Banner 1'),
+            ),
+            Center(
+              child: Text('Banner 2'),
+            ),
+            Center(
+              child: Text('Banner 3'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
